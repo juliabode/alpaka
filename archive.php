b3a6ef6c1a160f7e9096be47b0de97df5d5acc25
Julia Bode <julia.bode@lulisaur.us>
2012-04-12 22:39:20 +0200

    Style article excerpts in archive page.

--- archive.php
+++ archive.php
@@ -34,17 +34,27 @@
 
       <?php while (have_posts()) : the_post(); ?>
 
-        <article <?php post_class() ?>>
+    <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+          <div class="inner-wrapper">
 
-            <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+            <?php if ( has_post_thumbnail() ) {
+                    the_post_thumbnail();
+                  } else { ?>
 
-            <?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
+            <img class="left" src="<?php bloginfo('template_directory'); ?>/timthumb.php?src=<?php echo getImageForThumb('1'); ?>&w=240&h=240&zc=1" width="240" height="240"/>
 
-            <div class="entry">
-              <?php the_content(); ?>
-            </div>
+            <?php } ?>
 
-        </article>
+      <div class="entry right">
+        <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+
+        <?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
+        <?php the_excerpt(); ?>
+      </div>
+
+      </div>
+
+    </article>
 
       <?php endwhile; ?>
 
