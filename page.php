54ab52b6fab0f4224c2ed0b850d910bf7723c6dc
Julia Bode <julia.bode@lulisaur.us>
2013-01-15 17:22:59 +0100

    Make style changes for pages.

--- page.php
+++ page.php
@@ -1,29 +1,29 @@
 <?php get_header(); ?>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-		<article class="post" id="post-<?php the_ID(); ?>">
 
-			<h2><?php the_title(); ?></h2>
+    <div class="article-wrap">
+        
+      <article class="post pages" id="post-<?php the_ID(); ?>">
+        <div class="inner-wrapper">
 
-			<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
+          <h2><?php the_title(); ?></h2>
 
-			<div class="entry">
+          <div class="entry">
 
-				<?php the_content(); ?>
+            <?php the_content(); ?>
 
-				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+            <?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
 
-			</div>
+          </div>
 
-			<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+          <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
 
-		</article>
-		
-		<?php comments_template(); ?>
+        </div>
+      </article>
 
-		<?php endwhile; endif; ?>
+    </div>
 
-<?php get_sidebar(); ?>
+  <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
