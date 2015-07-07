db05330efc79a26badfc9f717b4857b7072ff930
Julia Bode <julia.bode@hmmh.de>
2015-07-07 09:51:38 +0200

    Change styling of weeklies.

--- templates/content-single-woechentliches.php
+++ templates/content-single-woechentliches.php
@@ -1,19 +1,19 @@
 <article <?php post_class(); ?>>
     <?php   $thumb_id = get_post_thumbnail_id();
             $thumb_url = wp_get_attachment_image_src($thumb_id,'full', true); ?>
-    <header style="background-image:url(<?php echo $thumb_url[0]; ?>)">
-        <span class="categories">
-            <?php
-                $taxonomies = get_the_terms(get_the_ID(), 'type'); 
-                foreach ( $taxonomies as $taxonomy ) {
-                    echo $taxonomy->name;
-                }
-            ?></span>
-    </header>
+    <a href="<?php the_permalink(); ?>">
+        <header style="background-image:url(<?php echo $thumb_url[0]; ?>)">
+            <span class="categories">
+                <?php
+                    $taxonomies = get_the_terms(get_the_ID(), 'type'); 
+                    foreach ( $taxonomies as $taxonomy ) {
+                        echo $taxonomy->name;
+                    }
+                ?></span>
+        </header>
+    </a>
 
     <div class="post-content">
-        <h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-
         <div class="entry-content">
             <?php the_excerpt(); ?>
         </div>
