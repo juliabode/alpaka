88cf7324a573933c6474f5ce32fba05162a5539c
Julia Bode <julia.bode@hmmh.de>
2015-07-14 09:16:44 +0200

    Add content output on weeklies page.

--- page-weeklies.php
+++ page-weeklies.php
@@ -2,26 +2,33 @@
 /*
 Template Name: Weeklies Overview
 */
-
-$args = array( 'post_type' => 'weeklies', 'posts_per_page' => -1 );
-$loop = new WP_Query( $args );
-
 ?>
 
+<div class="large-8 medium-8 small-12 column">
+    <?php while (have_posts()) : the_post(); ?>
+      <?php get_template_part('templates/page', 'header'); ?>
+      <?php get_template_part('templates/content', 'page'); ?>
+    <?php endwhile; ?>
 
-<div class="large-8 medium-8 small-12 column weeklies-overview">
-    <?php if (!$loop->have_posts()) : ?>
-      <div class="alert alert-warning">
-        <?php _e('Sorry, no results were found.', 'roots'); ?>
-      </div>
-      <?php get_search_form(); ?>
-    <?php endif; ?>
+<?php
+  $args = array( 'post_type' => 'weeklies', 'posts_per_page' => -1 );
+  $loop = new WP_Query( $args );
+?>
 
-    <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
-        <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-          <li><?php get_template_part('templates/content', 'single-woechentliches'); ?></li>
-        <?php endwhile; ?>
-    </ul>
+  <div class="weeklies-overview">
+      <?php if (!$loop->have_posts()) : ?>
+        <div class="alert alert-warning">
+          <?php _e('Sorry, no results were found.', 'roots'); ?>
+        </div>
+        <?php get_search_form(); ?>
+      <?php endif; ?>
+
+      <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
+          <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+            <li><?php get_template_part('templates/content', 'single-woechentliches'); ?></li>
+          <?php endwhile; ?>
+      </ul>
+  </div>
 </div>
 
 <aside class="sidebar large-4 medium-4 small-12 column" role="complementary">
